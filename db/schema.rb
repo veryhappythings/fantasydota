8b53b05d432d44fa97a08640f99e624725ef394f
Mac <mac@veryhappythings.co.uk>
2014-07-13 20:24:44 +0100

    Add players and teams

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140707123636) do
+ActiveRecord::Schema.define(version: 20140713190108) do
+
+  create_table "players", force: true do |t|
+    t.integer  "team_id"
+    t.string   "name"
+    t.string   "handle"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "teams", force: true do |t|
+    t.string   "name"
+    t.string   "abbreviation"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "",    null: false
